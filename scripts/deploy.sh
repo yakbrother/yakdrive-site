3b32012129455b2920d7e41bbd47c1a2fa0bbc14
Tim Eaton <tim@reports-inc.com>
2017-06-18 20:31:26 -0400

    Added basic static page

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,8 @@
 
 set -eux
 
-scp -r ./www yakdrive:/var/www/yakdrive.io
+scp -r ./www/* yakdrive:/var/www/yakdrive.io
 
-scp ./config/nginx.conf yakdrive:/etc/nginx/sites-enabled/default
-ssh yakdrive -- service nginx restart
+# scp ./config/nginx.conf yakdrive:/etc/nginx/sites-enabled/default
+
+# ssh yakdrive -- service nginx restart
